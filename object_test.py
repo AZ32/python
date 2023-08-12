ef5e335bbbbeab3030715131e7a7c59af7270dcd
AZ32 <66645907+AZ32@users.noreply.github.com>
2023-08-11 20:31:20 -0700

    Object test, minimum and maximum confidence.

---
+++ object_test.py
@@ -0,0 +1,13 @@
+import cv2
+import mediapipe as mp
+
+mp_objectron = mp.solutions.objectron
+mp_drawing = mp.solutions.drawing_utils
+
+objectron = mp_objectron.Objectron(static_image_mode=False,
+                                   max_num_objects=3,
+                                   min_detection_confidence=0.5,
+                                   min_tracking_confidence=0.99,
+                                   model_name="Cup")
+
+# setup OpenCV videos
