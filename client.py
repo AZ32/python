c5a982fabdeb4638a1eaec16ee51837e6e3adf71
AZ32 <66645907+AZ32@users.noreply.github.com>
2023-09-22 20:32:08 -0700

    Testing programs

--- client.py
+++ client.py
@@ -1,5 +1,7 @@
 import requests
 import cv2
+import numpy as np
+from detection_visualizer import visualize_results, crop_image, attach_to_original
 
 API_URL = "http://127.0.0.1:5000/detect"
 
@@ -10,13 +12,36 @@ while vid_capture.isOpened():
     if not success:
         break
 
-    _, frame_encoded = cv2.imencode('.jpg', frame)
+    original_height, original_width, _ = frame.shape
+    cropped_area = crop_image(frame)
+    cropped_height, cropped_width, _ = cropped_area.shape
+
+    frame_resized = cv2.resize(cropped_area, (320, 320), interpolation=cv2.INTER_LINEAR)
+    frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
+
+    _, frame_encoded = cv2.imencode('.jpg', frame_rgb)
     response = requests.post(API_URL, files={"frame": frame_encoded.tobytes()})
 
-    frame_data = response.content
-    processed_frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), -1)
+    # print(f"TESTING: {response.content}")
 
-    cv2.imshow("Processed Video", processed_frame)
+    detection_results = response.json()
+
+    boxes = np.array(detection_results["boxes"])
+    classes = np.array(detection_results["classes"])
+    confidence_scores = np.array(detection_results["confidence_scores"])
+
+    # Annotate the frame
+    annotated_frame = visualize_results(frame_resized, boxes, classes, confidence_scores)
+
+    annotated_frame = cv2.resize(annotated_frame, (cropped_width, cropped_height), interpolation=cv2.INTER_LINEAR)
+    frame = attach_to_original(annotated_frame, frame)
+
+    frame = cv2.resize(frame, (original_width // 4, original_height // 4), interpolation=cv2.INTER_LINEAR)
+
+    # frame_data = response.content
+    # processed_frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), -1)
+
+    cv2.imshow("Processed Video", frame)
     key = cv2.waitKey(1)
 
     if key == ord("q"):
--- model_server.py
+++ model_server.py
@@ -16,20 +16,20 @@ print("Model loaded successfully!")
 
 @app.route("/detect", methods=["POST"])
 def detect_objects():
-    frame_data = request.file["frame"].read()
+    frame_data = request.files["frame"].read()
     frame = cv2.imdecode(np.frombuffer(frame_data, np.uint8), -1)
 
-    original_height, original_width, _ = frame.shape
-    cropped_area = crop_image(frame)
-    cropped_height, cropped_width, _ = cropped_area.shape
+    # original_height, original_width, _ = frame.shape
+    # cropped_area = crop_image(frame)
+    # cropped_height, cropped_width, _ = cropped_area.shape
 
-    # if not flag:
-    #     flag = True
-    #     print(f"{cropped_height}, {cropped_width}")
+    # # if not flag:
+    # #     flag = True
+    # #     print(f"{cropped_height}, {cropped_width}")
 
-    frame_resized = cv2.resize(cropped_area, (320, 320), interpolation=cv2.INTER_LINEAR)
-    frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
-    frame_tensor = tf.convert_to_tensor(frame_rgb, dtype=tf.uint8)[tf.newaxis, ...]
+    # frame_resized = cv2.resize(cropped_area, (320, 320), interpolation=cv2.INTER_LINEAR)
+    # frame_rgb = cv2.cvtColor(frame_resized, cv2.COLOR_BGR2RGB)
+    frame_tensor = tf.convert_to_tensor(frame, dtype=tf.uint8)[tf.newaxis, ...]
 
     # Detection
     results = detector(frame_tensor)
@@ -43,16 +43,22 @@ def detect_objects():
     # get list of confidence scores
     confidence_scores = results['detection_scores'][0].numpy()
 
-    # Annotate the frame
-    annotated_frame = visualize_results(frame_resized, boxes, classes, confidence_scores)
+    return jsonify({
+        "boxes": boxes.tolist(),
+        "classes": classes.tolist(),
+        "confidence_scores": confidence_scores.tolist()
+    })
 
-    annotated_frame = cv2.resize(annotated_frame, (cropped_width, cropped_height), interpolation=cv2.INTER_LINEAR)
-    frame = attach_to_original(annotated_frame, frame)
+    # # Annotate the frame
+    # annotated_frame = visualize_results(frame_resized, boxes, classes, confidence_scores)
 
-    frame = cv2.resize(frame, (original_width // 4, original_height // 4), interpolation=cv2.INTER_LINEAR)
+    # annotated_frame = cv2.resize(annotated_frame, (cropped_width, cropped_height), interpolation=cv2.INTER_LINEAR)
+    # frame = attach_to_original(annotated_frame, frame)
 
-    _, buf = cv2.imencode(".jpg", frame)
-    return buf.tobytes()
+    # frame = cv2.resize(frame, (original_width // 4, original_height // 4), interpolation=cv2.INTER_LINEAR)
+
+    # _, buf = cv2.imencode(".jpg", frame)
+    # return buf.tobytes()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
