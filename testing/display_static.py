6f8eaabbdd9b42b0913e4abe180a52c7e73bd129
Andrew <azh3ng2020@gmail.com>
2024-01-03 20:00:43 -0800

    More display code

--- testing/display_static.py
+++ testing/display_static.py
@@ -6,14 +6,15 @@ palette = Palette(3)
 palette[0] = 0x000000 # Black
 palette[1] = 0xFFFFFF # White
 palette[2] = 0xFF0000 # Red
-
+bitmap=Bitmap(320,172,2)
 for x in range(320):
     for y in range(172):
         bitmap[x, y] = 1 if 50 <= x <= 270 and 50 <= y <= 122 else 0
 
 # TODO: get BDF font to include here
-font_file = "fonts/LeagueSpartan-Bold-12.bdf"
-font = bitmap_font.load_font(font_file)
+
+font = bitmap_font.load_font("Minecraft-12.bdf")
+
 
 text_area = adafruit_display_text.label.Label(font, text="Hello!", color=0xFFFFFF)
 text_area.x = 0
